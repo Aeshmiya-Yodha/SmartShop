ac6ef49ba44a9b611784efb4ff0965cc080a9107
Manyat Jindal <mnyt@Manyats-MacBook-Air.local>
2026-07-08 21:48:06 +0530

    first commit

---
+++ Model/User.cs
@@ -0,0 +1,11 @@
+
+public class Users
+{
+    public int UserId {get; set; }
+    public required string  FirstName {get; set;}
+    public required string LastName {get; set ; }
+    public required string ContactNumber {get; set;} // authentication will be handled by the Contact number or Password
+    
+    // public 
+
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Diagnostics;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
+var app = builder.Build(); 
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.Use(async (context, next) =>
+{
+    try
+    {
+        await next();
+    }
+    catch (Exception ) //If you will be add logging then think of adding the ex and using it 
+    {
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsync("Something went wrong! ");
+        
+    }
+});
+}
+
+// app.UseAuthentication();
+// app.UseAuthorization();
