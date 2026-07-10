dfd7f3cd99280bda6c5ef934cfe4f89c0aa1448a
Manyat Jindal | MAQ Software <manyatj@maqsoftware.com>
2026-07-10 10:45:42 +0530

    modified the Order entity

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,9 +1,19 @@
+public enum OrderStatus
+{
+    Pending,
+    Processing,
+    Shipped,
+    Delivered,
+    Cancelled
+}
+
 public class Order
 {
     public int OrderId{get; set; }
     public ICollection<OrderItem> OrderItems { get; set;}= new List<OrderItem>();
     public DateTime OrderDate {get; set;} = DateTime.UtcNow;
-    public  string Status {get ;set; } = null!;
+    public  OrderStatus Status {get ;set; } = OrderStatus.Pending;
+    public bool isPaid {get; set;} = false;
     public  decimal TotalAmount{get; set;} 
     public int UserId {get; set;}
     public User User {get; set;} = null!;
