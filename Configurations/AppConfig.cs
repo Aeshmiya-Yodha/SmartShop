3505e487c5b887dab7b94f525a437d58ab4e6710
Manyat Jindal <mnyt@Manyats-MacBook-Air.local>
2026-07-13 01:10:31 +0530

    Added Migrations , connections

--- Configurations/AppConfig.cs
+++ Configurations/AppConfig.cs
@@ -2,25 +2,23 @@ using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<Address> Addresses {get; set;}
-    public DbSet<Cart> Carts {get; set;}
-    public DbSet<CartItem> CartItems {get; set;}
-    public DbSet<Order> Orders {get; set;}
-    public DbSet<OrderItem> OrderItems {get; set;}
-    public DbSet<Product> Products{get; set;}
-    public DbSet<ProductCategory> ProductCategories{get; set;}
-    public DbSet<User> Users { get; set;}
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductCategory> ProductCategories { get; set; }
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
     }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+    : base(options)
     {
-        base.OnConfiguring(optionsBuilder);
-        optionsBuilder.UseNpgsql();
     }
-
 
 }
